d8e7c9ea1787e217276e09e3507ed8065e2943c6
GTM <growtomaxco@gmail.com>
2024-06-28 21:29:56 +0530

    Update sidebar.tsx

--- packages/blog-starter-kit/themes/enterprise/components/sidebar.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/sidebar.tsx
@@ -1,17 +1,23 @@
 import * as DialogPrimitive from '@radix-ui/react-dialog';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
+import { PublicationNavbarItem } from '../generated/graphql';
+import { Button } from './button';
 import { useAppContext } from './contexts/appContext';
+import CloseSVG from './icons/svgs/CloseSVG';
 import { PublicationLogo } from './publication-logo';
+import { SocialLinks } from './social-links';
 
 type Props = {
   toggleSidebar: () => void;
+  navbarItems: (PublicationNavbarItem & { url: string })[];
 };
 
 function PublicationSidebar(props: Props) {
-  const { toggleSidebar } = props;
+  const { toggleSidebar, navbarItems } = props;
   const [isMounted, setIsMounted] = useState(false);
   const { publication } = useAppContext();
+  const hasSocialLinks = !Object.values(publication.links!).every((val) => val === '');
 
   useEffect(() => {
     setIsMounted(true);
@@ -21,9 +27,9 @@ function PublicationSidebar(props: Props) {
     <DialogPrimitive.Root open>
       <DialogPrimitive.Portal>
         <DialogPrimitive.Overlay
-          className={`fixed inset-0 z-50 bg-slate-900 opacity-0 transition-opacity duration-300 ease-out ${
+          className={fixed inset-0 z-50 bg-slate-900 opacity-0 transition-opacity duration-300 ease-out ${
             isMounted && 'opacity-50'
-          }`}
+          }}
         />
         <DialogPrimitive.Content
           onEscapeKeyDown={() => {
@@ -32,9 +38,9 @@ function PublicationSidebar(props: Props) {
           onPointerDownOutside={() => {
             toggleSidebar();
           }}
-          className={`${
+          className={${
             !isMounted ? '-translate-x-96' : 'translate-x-0'
-          } fixed bottom-0 left-0 top-0 z-50 flex w-80 transform flex-col bg-white shadow-2xl duration-300 ease-out dark:border-neutral-800 dark:bg-neutral-950`}
+          } fixed bottom-0 left-0 top-0 z-50 flex w-80 transform flex-col bg-white shadow-2xl duration-300 ease-out dark:border-neutral-800 dark:bg-neutral-950}
         >
           <div className="blog-sidebar-header w-full shrink-0 py-6">
             <div className="flex items-center justify-between pl-8 pr-4">
@@ -43,14 +49,15 @@ function PublicationSidebar(props: Props) {
               </div>
 
               <DialogPrimitive.Close asChild>
-                <button
+                <Button
+                  type="outline"
+                  label=""
+                  icon={<CloseSVG className="h-5 w-5 fill-current" />}
                   className="rounded-xl !border-transparent !px-3 !py-2 hover:bg-neutral-800 dark:text-white"
                   onClick={() => {
                     toggleSidebar();
                   }}
-                >
-                  Close
-                </button>
+                />
               </DialogPrimitive.Close>
             </div>
           </div>
@@ -76,6 +83,7 @@ function PublicationSidebar(props: Props) {
                 </li>
               </ul>
             </section>
+            
           </div>
         </DialogPrimitive.Content>
       </DialogPrimitive.Portal>
